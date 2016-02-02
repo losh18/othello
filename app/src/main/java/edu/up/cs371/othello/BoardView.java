09d3bedd8cb8e89ce5a679c8b46ef710409bfb75
Kieran L <losh18@up.edu>
2016-02-01 17:02:43 -0800

    Added comments to BoardView file.

--- app/src/main/java/edu/up/cs371/othello/BoardView.java
+++ app/src/main/java/edu/up/cs371/othello/BoardView.java
@@ -13,7 +13,14 @@ import android.widget.Button;
 import android.widget.TextView;
 
 /**
- * Created by AaronLeung on 1/31/16.
+ * @author Aaron Leung
+ * @author Ryan Kane
+ * @author Austin Moehnke
+ * @author Kieran Losh
+ *
+ * Custom surfaceView class designed to act as the board in othello. The board has an array of pieces,
+ * and draws them on its canvas. The board handles touch inputs and the presses of the confirm button.
+ *
  */
 public class BoardView extends SurfaceView implements View.OnTouchListener, View.OnClickListener{
 
@@ -84,6 +91,9 @@ public class BoardView extends SurfaceView implements View.OnTouchListener, View
     }
 
     @Override
+    /**
+     * Draws the pieces on the board using the 2-d array of pieces.
+     */
     public void onDraw(Canvas canvas) {
         //get surfaceview size
         float width = this.getWidth();
@@ -127,9 +137,14 @@ public class BoardView extends SurfaceView implements View.OnTouchListener, View
 
     }
     @Override
+    /**
+     * onTouch handles the placement of pieces, it converts x,y inputs to i,j array values,
+     * then toggles the piece at i,j to be visible/invisible.
+     */
     public boolean onTouch(View v, MotionEvent event) {
         //make sure that we only process one touch per drag
         if (event.getDownTime() == this.downTime){
+            //if it's the same press, ignore the touch event
             return false;
         }
         this.downTime = event.getDownTime();
@@ -149,40 +164,65 @@ public class BoardView extends SurfaceView implements View.OnTouchListener, View
         int i = (int) x;
         int j = (int) y;
 
+        //if there isn't a piece where clicked, make the piece visible.
+        //if the is a piece, make it invisible
         pieces[i][j].setEmpty(!pieces[i][j].isEmpty());
+        //set the color of the piece clicked to the current turn's color
         pieces[i][j].setColor(color);
+        //remove previous piece placed : you can only place one piece per turn
+        //check to make sure it's not the first move (lastX = -1) or  that the piece you're removing
+        //isn't the same piece that was just placed
         if(lastX != -1 && !(lastX == i && lastY == j)) {
             pieces[lastX][lastY].setEmpty(true);
         }
+        //set last variable so we can remove the piece if another one is placed
         lastX = i;
         lastY = j;
         counter.setText("" + i + ", " + j);
         //Log.i("board", "" + i + " " + j);
 
+        //tell the board to redraw
         this.invalidate();
 
 
         return true;
     }
 
+    /**
+     * setTextView takes a reference to a TextView so that the board can modify that view.
+     * If it is not passed from main, BoardView cannot get a reference to the TextView.
+     * @param counter - reference to the TextView that will display the count
+     */
     public void setTextView(TextView counter) {
         this.counter = counter;
     }
 
+    /**
+     * Takes button references that need to be monitored by BoardView (using the onClick listener)
+     * and saves them as local private variables.
+     * @param confirm1 - Confirm button one at the bottom of the screen
+     * @param confirm2 - Confirm button two is at the top of the screen
+     */
     public void setConfirmButtons(Button confirm1, Button confirm2){
         this.confirmButton1 = confirm1;
         this.confirmButton2 = confirm2;
     }
 
     @Override
+    /**
+     * Detects when the confirm buttons are pressed, and changes the turn to the other player when
+     * the current player presses their confirm button.
+     */
     public void onClick(View v) {
+        //Black player presses their confirm button and it is their turn:
         if (v.getId() == R.id.confirmButton && color == Piece.BLACK) {
-            Log.i("Button1", "Clicked");
-            lastX = -1;
+            //Log.i("Button1", "Clicked");
+            lastX = -1; //we don't want to overwrite the piece placed, so set the lastX = -1
             color = Piece.WHITE;
         }
+        //White player presses their confirm button and it is their turn:
         if (v.getId() == R.id.confirmTopButton && color == Piece.WHITE){
-            Log.i("Button2", "Clicked");
+            //Log.i("Button2", "Clicked");
             lastX = -1;
             color = Piece.BLACK;
         }
